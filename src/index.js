27c597b12867b7a71c794231cde588393a480863
differentsyntax <nothingreallyfaraway@gmail.com>
2020-04-19 12:28:59 -0400

    Simplified/consized syntax

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-class Message extends React.Component {
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100,
+};
+
+class SkiDayCounter extends Component {
   render() {
+    const { total, powder, backcountry, goal } = this.props;
+    // above line can be used to avoid writing this.props for each of them
     return (
-      <div>
-        <h1 style={{ color: this.props.color }}>{this.props.msg}</h1>
-        <p> I'll check back in {this.props.minutes} minutes</p>
-      </div>
+      <section>
+        <div>
+          <p> Total Days: {total}</p>
+        </div>
+
+        <div>
+          <p> Powder Days: {powder}</p>
+        </div>
+
+        <div>
+          <p> Backcountry Days: {backcountry}</p>
+        </div>
+
+        <div>
+          <p> Goal Days: {goal}</p>
+        </div>
+      </section>
     );
   }
 }
 
-ReactDOM.render(
-  <Message minutes={20} color="blue" msg="Hey" />,
+render(
+  <SkiDayCounter
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.goal}
+  />,
   document.getElementById("root")
 );
