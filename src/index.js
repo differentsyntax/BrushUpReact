3bdfba59ee91e524d700444222dc9772ab646b11
differentsyntax <nothingreallyfaraway@gmail.com>
2020-04-20 10:35:57 -0400

    concept of state, and setting it locally and globally

--- src/index.js
+++ src/index.js
@@ -7,28 +7,50 @@ const bookList = [
   {"title":"The Ugly Truth 3", "author":"Minisha Augustine", "pages": 250}
 ]
 
-const Book = ({ title, author, pages }) => {
+const Book = ({ title, author, pages, freeBookmark }) => {
   return (
     <div>
       <h1>{title}</h1>
       <p> By: {author}</p>
       <p> Pages: {pages}</p>
+      <p> Free Bookmark Today?: {freeBookmark} ? "Yes" : "No"} </p>
     </div>
   )
 }
 
-const Library = ({books}) => {
-  return (
-    <div>
-      {books.map(
-        (book,i) => <Book key = {i} 
-                          title = {book.title} 
-                          author = {book.author} 
-                          pages = {book.pages}>
-                    </Book>
-      )}
-    </div>
-  )
+class Library extends Component {
+  
+  state = { 
+    open: false,
+    freeBookmark: true
+   }
+  
+  toggle = () => {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }))
+  }
+
+  render(){
+    var style = {
+      backgroundColor: 'Orange'
+    }
+    const {books} = this.props // const books = this.props.books
+    return (
+      <div>
+        <h1 style = {style} >The library is {this.state.open ? "open" : "closed" }</h1>
+    <button onClick = {this.toggle}>{this.state.open ? "Shut Down" : "Open Library"}</button>
+        {books.map(
+          (book,i) => <Book key = {i} // unique identifier key for mapping
+                            title = {book.title} 
+                            author = {book.author} 
+                            pages = {book.pages}>
+                            freeBookmark = {this.state.freeBookmark}
+                      </Book>
+        )}
+      </div>
+    )
+  }
 }
 
 render(
