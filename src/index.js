48ed9d30d850e4448b2712c80508951ffe1338b3
differentsyntax <nothingreallyfaraway@gmail.com>
2020-04-20 06:05:30 -0400

    Concept of parent components, calling one function component from another

--- src/index.js
+++ src/index.js
@@ -1,49 +1,26 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100,
-};
-
-const getPercent = (decimal) => {
-  return decimal * 100 + "%";
-};
-
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total / goal);
-};
-
-const SkiDayCounter = ({ total, powder, backcountry, goal }) => {
+const Book = ({ title, author, pages }) => {
   return (
-    <section>
-      <div>
-        <p> Total Days: {total}</p>
-      </div>
-
-      <div>
-        <p> Powder Days: {powder}</p>
-      </div>
-
-      <div>
-        <p> Backcountry Days: {backcountry}</p>
-      </div>
-
-      <div>
-        <p> Goal Progress: {calcGoalProgress(total, goal)}</p>
-      </div>
-    </section>
+    <div>
+      <h1>{title}</h1>
+      <p> By: {author}</p>
+      <p> Pages: {pages}</p>
+    </div>
   );
 };
 
+const Library = () => {
+  return (
+    <div>
+      <Book title="The Ugly Truth" author="Minisha Augustine" pages={250}></Book>
+      <Book title="The Ugly Truth 2" author="Minisha Augustine" pages={250}></Book>
+      <Book title="The Ugly Truth 3" author="Minisha Augustine" pages={250}></Book>
+    </div>
+  );
+};
 render(
-  <SkiDayCounter
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal}
-  />,
+  <Library />, 
   document.getElementById("root")
-);
+  );
